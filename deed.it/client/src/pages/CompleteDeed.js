e6650461f357e512ae46dc8d478593e537808f81
Mike Evans <mike.x.evans@tescobank.com>
2018-03-29 14:36:48 +0100

    added node-based TLS

--- deed.it/client/src/pages/CompleteDeed.js
+++ deed.it/client/src/pages/CompleteDeed.js
@@ -8,10 +8,46 @@ class CompleteDeed extends Component {
 
   constructor (props ) {
     super(props);
-    this.state = { stage: 'reviewPhoto' };
+    this.state = { stage: 'choose' };
   }
 
-  renderWithVideo () {
+  renderChoice () {
+    const optionChosen = (event) => this.setState({...this.state, stage: event.target.id});
+    return (
+      <div>
+        <Instruction text='Choose how to report the deed done:' />
+        <fieldset>
+          <div>
+            <input type='radio' id='takePhoto' onChange={optionChosen}></input>
+            <label htmlFor='takePhoto'>Take a photo of the done deed</label>
+          </div>
+          <div>
+            <input type='radio' id='declaration' onChange={optionChosen}></input>
+            <label htmlFor='declaration'>Simply report it complete</label>
+          </div>
+        </fieldset>
+        <div>
+          <Button click={this.props.cancel} text='Not now' />
+        </div>
+      </div>
+    );
+  }
+
+  renderDeclaration () {
+    return (
+      <div>
+        I declare the deed done!
+        <div>
+          <Button click={this.props.submit} text='Report deed done' />
+        </div>
+        <div>
+          <Button click={this.props.cancel} text='Not now' />
+        </div>
+      </div>
+    );
+  }
+
+  renderVideo () {
     return (
       <div>
         <Instruction text='Click the picture to take a photo:' />
@@ -23,7 +59,7 @@ class CompleteDeed extends Component {
       </div>);
   }
 
-  renderWithPhoto () {
+  renderPhoto () {
     const imageSrc = `${process.env.PUBLIC_URL}/placeholder.png`;
     return (
       <div>
@@ -40,13 +76,24 @@ class CompleteDeed extends Component {
   }
 
   render () {
-    const imageUI = ( this.state.stage === 'takePhoto' ) ? 
-      this.renderWithVideo() :
-      this.renderWithPhoto();
+    let content;
+    switch(this.state.stage) {
+      case 'takePhoto':
+        content = this.renderVideo();
+        break;
+      case 'confirmPhoto':
+        content = this.renderPhoto();
+        break;
+      case 'declaration':
+        content = this.renderDeclaration();
+        break;
+      default:
+        content = this.renderChoice();
+    }
     return (
       <div>
-        <Header text='Report your deed done by taking a photo of it.' />
-        { imageUI }
+        <Header text='Report your deed done.' />
+        { content }
       </div>
     );
   }
--- deed.it/server/server.js
+++ deed.it/server/server.js
@@ -1,8 +1,45 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const http = require('http');
+const https = require('https');
+const io = require('socket.io');
+const bodyParser = require('body-parser');
 
-const root = path.resolve(__dirname, '../client/build');
+const config = {
+  port: 80,
+  securePort: 443
+};
+
+const certsRoot = path.resolve(__dirname, './.ssl');
+const webRoot = path.resolve(__dirname, '../client/build');
+
+const privateKey = fs.readFileSync(path.resolve(certsRoot, 'key.pem'));
+const certificate = fs.readFileSync(path.resolve(certsRoot, 'cert.pem'));
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  passphrase: '1234'
+};
+
 const app = express();
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+const socketsServer = require('socket.io')(httpServer);
 
-app.use(express.static(root));
-app.listen(80, () => console.log('Server started on port 80'));
+app.use(bodyParser.json({limit: '2mb'}));
+app.use(express.static(webRoot));
+
+app.post('/photo', (request, response) => {
+  console.log(`Photo received from id ${request.body.id}`);
+  socketsServer.emit('photo', photo);
+  response.status(200).end();
+});
+
+socketsServer.on('connection', (socket) => {
+  console.log('socket client connected');
+  socket.emit('config', config);
+})
+
+httpServer.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
+httpsServer.listen(config.securePort, () => console.log(`Secure server listening on ${config.securePort}`));
