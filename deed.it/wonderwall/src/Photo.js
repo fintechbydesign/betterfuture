a353cd102d0479f3c78408a0a66776fcea4e8ccf
Mike Evans <mike.x.evans@tescobank.com>
2018-03-31 15:29:00 +0100

    resize in wonderwall - working

---
+++ deed.it/wonderwall/src/Photo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Photo(props) {
+  return (<img src={props.src} alt='deed evidence' />);
+}
+
+export default Photo;
--- deed.it/wonderwall/src/WonderWall.js
+++ deed.it/wonderwall/src/WonderWall.js
@@ -1,17 +1,29 @@
 import io from 'socket.io-client';
+import Photo from './Photo';
 import React, { createRef, Component } from 'react';
 import './WonderWall.css';
+
+const debug = false;
+const delay = () => new Promise((resolve) => setTimeout(resolve));
 
 class WonderWall extends Component {
   constructor (props) {
     super(props);
+
+    this.addPhoto = this.addPhoto.bind(this);
+    this.replayPhotoEvent = this.replayPhotoEvent.bind(this);
+    this.reportNews = this.reportNews.bind(this);
+    this.resizeImage = this.resizeImage.bind(this);
+
     this.state = { photos: [] };
+
     this.canvas = createRef();
     this.image = createRef();
-    this.resizeImage = this.resizeImage.bind(this);
-    const socket = io();
-    socket.on('news', this.reportNews.bind(this));
-    socket.on('photo', this.addPhoto.bind(this));
+
+    const socketURL = debug ? 'https://localhost' : undefined;
+    const socket = io(socketURL);
+    socket.on('news', this.reportNews);
+    socket.on('photo', this.addPhoto);
   }
 
   reportNews (news) {
@@ -20,16 +32,21 @@ class WonderWall extends Component {
 
   resizeImage (src, width, height) {
     const canvas = this.canvas.current;
-    const image = this.image.current;
-    image.src = src;
-    canvas.width = image.width /2;
-    canvas.height = image.height / 2;
-    canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+    const img = this.image.current;
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext('2d').drawImage(img, 0, 0, width, height);
     return canvas.toDataURL('image/png');
   }
 
-  addPhoto (photo) {
+  async addPhoto (photo) {
     console.log('photo received: ', photo);
+    if (debug) {
+      localStorage.setItem('testPhoto', JSON.stringify(photo));
+    }
+    const img = this.image.current;
+    img.src = photo.src;
+    await delay(); // required to allow image to draw (invisibly!)
     photo.smallSrc = this.resizeImage(photo.src, 320, 240);
     this.setState(prevState => ({
       ...prevState,
@@ -37,13 +54,25 @@ class WonderWall extends Component {
     }));
   }
 
+  replayPhotoEvent () {
+    const photo = localStorage.getItem('testPhoto');
+    if (photo) {
+      this.addPhoto(JSON.parse(photo));
+    } else {
+      console.error('no test photo to replay')
+    }
+  }
+
   render () {
-    const photos = this.state.photos.map(photo => (<img src={photo.smallSrc} alt='deed evidence' />));
+    const numPhotos = this.state.photos.length;
+    const photos = this.state.photos.map((photo, index) => (<Photo src={photo.smallSrc} key={numPhotos - index - 1} />));
+    const debugElements = debug ? (<button onClick={this.replayPhotoEvent}>Test Photo</button>) : null;
     return (
       <div>
         <img src='wonderwall.png' alt='wonderwall background' className='WonderWall_background' />
         {photos}
-        <image ref={this.image} className='WonderWall_hide' />
+        {debugElements}
+        <img ref={this.image} alt="for resizing" className='WonderWall_hide'/>
         <canvas ref={this.canvas} className='WonderWall_hide' />
       </div>
     );
