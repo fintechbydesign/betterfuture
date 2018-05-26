b622d1c964ca82f214ffad3cb546f9e38c1f2ad6
Mike Evans <mike.x.evans@tescobank.com>
2018-05-26 22:06:53 +0100

    get location

--- deed.it/client/src/components/CompleteDeed.js
+++ deed.it/client/src/components/CompleteDeed.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
+const getLocation = () => new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => resolve(position.coords),
+      (err) => {
+        console.log(`Position error: ${err.code} : ${err.message}`);
+        resolve(null);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      });
+  }
+);
+
 class CompleteDeed extends Component {
 
   constructor (props) {
     super(props);
     this.completeDeed = this.completeDeed.bind(this);
+    this.toggleRecordLocation = this.toggleRecordLocation.bind(this);
     this.state = {
-      recordLocation: false
+      recordLocation: true
     }
   }
 
-  completeDeed () {
+  toggleRecordLocation () {
+    this.setState({
+      ...this.state,
+      recordLocation: !this.state.recordLocation
+    });
+  }
+
+  async completeDeed () {
     const {deed, imageData, navigateFn, user} = this.props;
+    const { recordLocation }= this.state;
+    const location = recordLocation ? await getLocation() : null;
+    alert( location);
     navigateFn();
   }
 
@@ -21,6 +47,10 @@ class CompleteDeed extends Component {
     return (
       <div>
         <Button click={this.completeDeed} text={this.props.text} />
+        <div>
+          <input type='checkbox' id='location' onChange={this.toggleRecordLocation} checked={this.state.recordLocation} />
+          <label htmlFor="location">Record your current location?</label>
+        </div>
       </div>
     );
   }
