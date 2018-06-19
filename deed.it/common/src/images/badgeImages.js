63421cedf705f8c4453d1fa6130c4b9260480c64
parroyo <pablo.x.arroyo@tescobank.com>
2018-06-19 16:51:13 +0100

    fixed badgeImages

--- deed.it/common/src/images/badgeImages.js
+++ deed.it/common/src/images/badgeImages.js
@@ -6,10 +6,10 @@ import socialFirst from './social bite star.svg';
 import socialSecond from './social bite ambassador.svg';
 
 export default {
-  [`Help Edinburgh's Homeless_first`]: communityFirst,
-  [`Help Edinburgh's Homeless_second`]: communitySecond,
-  ['Help Edinburgh Go Green_first']: greenFirst,
-  ['Help Edinburgh Go Green_second']: greenSecond,
-  [`Help Edinburgh's Homeless_first`]: socialFirst,
-  [`Help Edinburgh's Homeless_second`]: socialSecond
+  'Help Edinburgh\'s Community Spirit_first': communityFirst,
+  'Help Edinburgh\'s Community Spirit_second': communitySecond,
+  'Help Edinburgh Go Green_first': greenFirst,
+  'Help Edinburgh Go Green_second': greenSecond,
+  'Help Edinburgh\'s Homeless_first': socialFirst,
+  'Help Edinburgh\'s Homeless_second': socialSecond
 };
