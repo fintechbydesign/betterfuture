969c4fea460abfdd4f1f63dd571b5e129891564e
Mike Evans <mike.x.evans@tescobank.com>
2018-06-22 16:35:56 +0100

    badge styling

--- deed.it/common/src/images/badges.js
+++ deed.it/common/src/images/badges.js
@@ -6,10 +6,46 @@ import socialFirst from './social bite star.svg';
 import socialSecond from './social bite ambassador.svg';
 
 export default {
-  'Help Edinburgh\'s Community Spirit_first': communityFirst,
-  'Help Edinburgh\'s Community Spirit_second': communitySecond,
-  'Help Edinburgh Go Green_first': greenFirst,
-  'Help Edinburgh Go Green_second': greenSecond,
-  'Help Edinburgh\'s Homeless_first': socialFirst,
-  'Help Edinburgh\'s Homeless_second': socialSecond
+  'Help Edinburgh\'s Community Spirit_first': {
+    icon: communityFirst,
+    style: {
+      color: '#0E88FC'
+    },
+    text: "Karma King"
+  },
+  'Help Edinburgh\'s Community Spirit_second': {
+    icon: communitySecond,
+    style: {
+      color: '#0E88FC'
+    },
+    text: "Diamond Geezer"
+  },
+  'Help Edinburgh Go Green_first': {
+    icon: greenFirst,
+    style: {
+      color: '#20BCA4'
+    },
+    text: "Zero Waste Champion"
+  },
+  'Help Edinburgh Go Green_second': {
+    icon: greenSecond,
+    style: {
+      color: '#20BCA4'
+    },
+    text: "Eco Warrior"
+  },
+  'Help Edinburgh\'s Homeless_first': {
+    icon: socialFirst,
+    style: {
+      color: '#FF6112'
+    },
+    text: "SocialBite Star"
+  },
+  'Help Edinburgh\'s Homeless_second': {
+    icon: socialSecond,
+    style: {
+      color: '#FF6112'
+    },
+    text: "SocialBite Ambassador"
+  }
 };
