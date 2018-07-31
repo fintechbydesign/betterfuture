01e8240be7188a0da1f9124c716dc2a8f671c26c
Mike Evans <mike.x.evans@tescobank.com>
2018-07-31 15:40:16 +0100

    ios9 image fix 2

--- deed.it/client/src/components/Image.js
+++ deed.it/client/src/components/Image.js
@@ -13,19 +13,17 @@ function createSrc (props) {
   }
 }
 
+function isEmbeddedSvg (src) {
+  return src.includes('image/svg+xml');
+}
+
 function Image (props) {
   const alt = props.alt ? props.alt : 'placeholder';
   const className = props.className ? props.className : 'Image-default';
   const src = createSrc(props);
-  /*
-  return (
-    <img alt={alt} className={className} crossOrigin='anonymous' onClick={props.onClick} onLoad={props.onLoad} src={src} />
-  );
-  // safari8-9 compatible tag, instead of image, for embedded SVG
-  */
-  return (
-    <object type="image/svg+xml" alt={alt} className={className} crossOrigin='anonymous' onClick={props.onClick} onLoad={props.onLoad} data={src}></object>
-  );
+  return (isEmbeddedSvg(src))
+    ? (<object type="image/svg+xml" alt={alt} className={className} crossOrigin='anonymous' onClick={props.onClick} onLoad={props.onLoad} data={src}></object>)
+    : (<img alt={alt} className={className} crossOrigin='anonymous' onClick={props.onClick} onLoad={props.onLoad} src={src} />);
 }
 
 export default Image;
